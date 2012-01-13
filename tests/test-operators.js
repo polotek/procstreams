91da5c5c35c788607bab8e0c3611b8a9b930ce65
Marco Rogers <marco.rogers@gmail.com>
2012-01-13 02:44:26 -0800

    update some tests

--- tests/test-operators.js
+++ tests/test-operators.js
@@ -7,14 +7,7 @@ exec('fail || echo "pass"', function(err, output) {
   if(err) { throw err }
   assert.equal('pass', output.trim())
 
-  var collect = function(proc, args) {
-    var data = ''
-    proc.stdout.on('data', function(d) { data += d })
-    proc.stdout.on('end', function() { console.log("\n---- " + proc._args.cmd + "\n\n" + data) })
-  }
-
   $p('fail', opts).or('echo "pass"', opts)
-    //collect(c)
     .on('start', function() {
       this.stdout.on('data', function(output) {
         assert.equal('pass', output.toString().trim())
--- tests/test-simple.js
+++ tests/test-simple.js
@@ -1,26 +1,26 @@
 var assert = require('assert')
-	, exec = require('child_process').exec
-	, $p = require(__dirname + '/..')
+  , exec = require('child_process').exec
+  , $p = require(__dirname + '/..')
 
 var version = process.version
 exec('node --version', function(err, output){
-	if(err) { throw err }
+  if(err) { throw err }
 
-	assert.equal(version, output.trim())
+  assert.equal(version, output.trim())
 
-	var opts = { out: false }
-	$p('node --version', opts)
-		.stdout.on('data', function(output) {
-			assert.equal(version, output.toString().trim())
-		})
+  var opts = { out: false }
+  $p('node --version', opts)
+    .stdout.on('data', function(output) {
+      assert.equal(version, output.toString().trim())
+    })
 
-	$p('node',  '--version', opts)
-		.stdout.on('data', function(output) {
-			assert.equal(version, output.toString().trim())
-		})
+  $p('node',  '--version', opts)
+    .stdout.on('data', function(output) {
+      assert.equal(version, output.toString().trim())
+    })
 
-	$p('node',  ['--version'], opts)
-		.stdout.on('data', function(output) {
-			assert.equal(version, output.toString().trim())
-		})
+  $p('node',  ['--version'], opts)
+    .stdout.on('data', function(output) {
+      assert.equal(version, output.toString().trim())
+    })
 })
