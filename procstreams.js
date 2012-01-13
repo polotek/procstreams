3141638807badaacd5ee3679457ecf14154771fc
Marco Rogers <marco.rogers@gmail.com>
2012-01-13 03:59:37 -0800

    it sort works

--- procstreams.js
+++ procstreams.js
@@ -1,5 +1,8 @@
-var spawn = require('child_process').spawn
-  , slice = Array.prototype.slice;
+var slice = Array.prototype.slice
+  , EventEmitter = require('events').EventEmitter
+  , spawn = require('child_process').spawn
+  , inherits = require('inherits')
+  , utils = require('./protochains');
 
 function procPipe(dest, options) {
   options = options || {}
@@ -24,14 +27,6 @@ function procPipe(dest, options) {
 
     source.stderr.pipe(dest_stderr);
   }
-
-  var onexit = function(exit, signal) {
-    // emitting end on the source pipes should trigger cleanup routines
-    source.stdout.emit('end');
-    source.stderr.emit('end');
-  }
-
-  source.on('exit', onexit);
 
   dest.emit('pipe', source);
 
@@ -71,7 +66,7 @@ function normalizeArguments(cmd, args, opts, callback) {
 
     parsedArgs = parseArgs(cmd);
     cmd = parsedArgs.shift();
-    
+
     if(Array.isArray(args)) {
       parsedArgs = parsedArgs.concat(args);
     }
@@ -95,68 +90,88 @@ function normalizeArguments(cmd, args, opts, callback) {
 function procStream(cmd, args, opts, callback) {
   if(!cmd) { throw new Error('Missing command'); }
 
-  if((cmd === process || typeof cmd.spawn == 'function') && typeof cmd.pipe == 'function') {
-    return cmd;
+  var proc = null, o = null;
+
+  // this is a process object
+  if((cmd === process || typeof cmd.spawn == 'function')) {
+    // this is already procstream
+    if(typeof cmd.pipe == 'function') {
+      return cmd;
+    } else {
+    // this is a process that needs to be enhanced
+      proc = procStream.enhance(cmd);
+    }
+  } else {
+  // get the args to create a new procstream
+    o = normalizeArguments(cmd, args, opts, callback);
+    cmd = o.cmd;
+    args = o.args;
+    opts = o.opts;
+    callback = o.callback;
+
+    proc = spawn(cmd, args, opts);
+    proc = procStream.enhance(proc);
+
+    proc._args = o;
   }
 
-  var o = normalizeArguments(cmd, args, opts, callback);
-  cmd = o.cmd;
-  args = o.args;
-  opts = o.opts;
-  callback = o.callback;
+  if(opts.out !== false) {
+    proc.out();
+  }
 
-  var child = spawn(cmd, args, opts);
+  if(typeof callback == 'function') { proc.on('exit', callback); }
 
-  child.out = function out() {
-    if(child._out) { return; }
-    child._out = true;
+  process.nextTick(function() { proc.emit('start'); });
+  return proc;
+}
+procStream.enhance = utils.enhance;
+procStream._prototype = {
+  out: function out() {
+    if(this._out) { return; }
+    this._out = true;
 
     var opts = { end: false }
-    child.stdout.pipe(process.stdout, opts);
-    child.stderr.pipe(process.stderr, opts);
+    this.stdout.pipe(process.stdout, opts);
+    this.stderr.pipe(process.stderr, opts);
 
     return this;
   }
+  , and: function and() {
+    var args = slice.call(arguments)
+      , dest = new procPromise(args);
 
-  child.and = function and() {
-    var source = child
-     , args = slice.call(arguments);
-
-    source.on('exit', function(exit, signal) {
-      if(exit === 0) {
-        process.nextTick(function() {
-          procStream.apply(null, args);
-        });
+    this.on('exit', function(code, signal) {
+      if(code === 0) {
+        dest.resolve(args);
       }
     });
+
+    return dest;
   }
+  , or: function or() {
+    var args = slice.call(arguments)
+      , dest = new procPromise(args);
 
-  child.or = function or() {
-    var source = child
-     , args = slice.call(arguments);
-
-    source.on('exit', function(exit, signal) {
-      if(exit !== 0) {
-        process.nextTick(function() {
-          procStream.apply(null, args);
-        });
+    this.on('exit', function(code, signal) {
+      if(code !== 0) {
+        dest.resolve();
       }
     });
+
+    return new procPromise(args);
   }
+  , then: function then() {
+    var args = slice.call(arguments)
+      , dest = new procPromise(args);
 
-  child.then = function then() {
-    var source = child
-      , args = slice.call(arguments);
-
-    source.on('exit', function(exit, signal) {
-      process.nextTick(function() {
-        procStream.apply(null, args);
-      });
+    this.on('exit', function(code, signal) {
+      dest.resolve();
     });
-  }
 
-  child.pipe = function(cmd, args, options) {
-    var source = child
+    return new procPromise(args);
+  }
+  , pipe: function(cmd, args, options) {
+    var source = this
       , dest = procStream.apply(null, arguments);
 
     if(typeof args != 'string' && !Array.isArray(args)) {
@@ -164,14 +179,27 @@ function procStream(cmd, args, opts, callback) {
     }
     return procPipe.call(source, dest, options);
   }
-
-  if(opts.out !== false) {
-    child.out();
-  }
-
-  if(typeof callback == 'function') { child.on('exit', callback); }
-
-  return child;
 }
+inherits(procStream, EventEmitter, procStream._prototype);
+
+function procPromise(args) {
+  this._args = args;
+
+  this.resolve = procPromise.prototype.resolve.bind(this);
+  this.reject = procPromise.prototype.reject.bind(this);
+}
+procPromise._prototype = {
+  resolve: function() {
+    process.nextTick(function() {
+      var proc = procStream.apply(null, this._args);
+      proc._events = utils.mixin({}, this._events);
+      return proc;
+    }.bind(this));
+  }
+  , reject: function() {
+
+  }
+}
+inherits(procPromise, procStream, procPromise._prototype);
 
 module.exports = procStream;
