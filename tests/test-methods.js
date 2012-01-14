f61596a18eb62be7dfb955fd78ca18da0e0cc00e
Marco Rogers <marco.rogers@gmail.com>
2012-01-14 01:42:45 -0800

    tests for collecting data

---
+++ tests/test-methods.js
@@ -0,0 +1,21 @@
+var assert = require('assert')
+  , fs = require('fs')
+  , timer = require(__dirname + '/timer')
+  , $p = require(__dirname + '/..')
+  , opts = { out: false }
+
+var processStdout = ''
+process.stdout.on('data', function(d) {
+  processStdout += d
+})
+
+fs.readFile('tests/fixtures/long.txt', function(err, fileData) {
+  if(err) { throw err }
+
+  var t = timer()
+  $p('cat tests/fixtures/long.txt', opts)
+    .data(function(stdout, stderr) {
+      t.stop()
+      assert.equal(fileData, stdout)
+    })
+})
--- tests/test-operators.js
+++ tests/test-operators.js
@@ -8,72 +8,62 @@ exec('echo && echo pass', function(err, output) {
   if(err) { throw err }
   assert.equal('pass', output.trim())
 
-  var t = timer();
+  var t = timer()
   $p('echo', opts).and('echo pass', opts)
-    .on('start', function() {
-      this.stdout.on('data', function(output) {
-        t.stop();
-        assert.equal('pass', output.toString().trim())
-      })
-    });
+    .data(function(output) {
+      t.stop()
+      assert.equal('pass', output.toString().trim())
+    })
 })
 
 exec('fail || echo pass', function(err, output) {
   if(err) { throw err }
   assert.equal('pass', output.trim())
 
-  var t = timer();
+  var t = timer()
   $p('fail', opts).or('echo pass', opts)
-    .on('start', function() {
-      this.stdout.on('data', function(output) {
-        t.stop();
-        assert.equal('pass', output.toString().trim())
-      })
-    });
+    .data(function(output) {
+      t.stop()
+      assert.equal('pass', output.toString().trim())
+    })
 })
 
 exec('fail; echo pass', function(err, output) {
   if(err) { throw err }
   assert.equal('pass', output.trim())
 
-  var t = timer();
+  var t = timer()
   $p('fail', opts).then('echo pass', opts)
-    .on('start', function() {
-      this.stdout.on('data', function(output) {
-        t.stop();
-        assert.equal('pass', output.toString().trim())
-      })
-    });
+    .data(function(output) {
+      t.stop()
+      assert.equal('pass', output.toString().trim())
+    })
 })
 
 exec('echo && echo pass && echo pass2', function(err, output) {
   if(err) { throw err }
   //assert.equal('pass2', output.trim())
 
-  var t = timer();
+  var t = timer()
   $p('echo', opts)
     .and('echo pass', opts)
     .and('echo pass2', opts)
-      .on('start', function() {
-        this.stdout.on('data', function(output) {
-          t.stop();
-          assert.equal('pass2', output.toString().trim());
-        });
-      });
+      .data(function(output) {
+        t.stop()
+        assert.equal('pass2', output.trim())
+      })
 })
 
 exec('fail || echo pass && echo pass2', function(err, output) {
   if(err) { throw err }
   //assert.equal('pass2', output.trim())
 
-  var t = timer();
+  var t = timer()
   $p('fail', opts)
     .or('echo pass', opts)
     .and('echo pass2', opts)
-      .on('start', function() {
-        this.stdout.on('data', function(output) {
-          t.stop();
-          assert.equal('pass2', output.toString().trim())
-        })
-      });
+      .data(function(output) {
+        t.stop()
+        assert.equal('pass2', output.toString().trim())
+      })
 })
