855659cbc67a438ee795cdaf9627db3eb9f9e0ff
James Halliday <mail@substack.net>
2012-01-30 01:11:50 -0800

    failing test for pipe(stream)

--- tests/test-pipe.js
+++ tests/test-pipe.js
@@ -2,6 +2,7 @@ var assert = require('assert')
   , timer = require('./timers').timer
   , exec = require('child_process').exec
   , $p = require(__dirname + '/..')
+  , Stream = require('stream').Stream
 
 exec('cat tests/fixtures/3lines.txt | wc -l', function(err, output) {
   if(err) { throw err }
@@ -28,4 +29,25 @@ exec('cat tests/fixtures/10lines.txt | grep "even" | wc -l'
           t.stop()
           assert.equal('5', output.toString().trim())
         })
+})
+
+exec('cat tests/fixtures/10lines.txt | grep "even"'
+  , function(err, output) {
+    if(err) { throw err }
+    
+    var t = timer()
+    var out = new Stream
+    out.writable = true
+    
+    var data = ''
+    out.on('data', function (buf) { data += buf })
+    
+    out.on('end', function () {
+      assert.equal(data, output)
+      t.stop()
+    })
+    
+    $p('cat tests/fixtures/10lines.txt')
+      .pipe('grep even')
+      .pipe(out)
 })
