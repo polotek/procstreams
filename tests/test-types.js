112f255fe2440afbcc42155db062e63b81a9417b
Marco Rogers <marco.rogers@gmail.com>
2012-08-11 08:27:02 -0400

    update type tests. things are a little weird here

--- tests/test-types.js
+++ tests/test-types.js
@@ -1,4 +1,5 @@
 var assert = require('assert')
+  , Stream = require('stream')
   , fs = require('fs')
   , cp = require('child_process')
   , exec = cp.exec
@@ -16,18 +17,26 @@ assert.ok($p.isProcess(child2))
 assert.ok(!$p.is(process))
 assert.ok(!$p.is(child));
 
-var file = fs.createReadStream('tests/fixtures/3lines.txt')
+var stream = new Stream()
+  , file = fs.createReadStream('tests/fixtures/3lines.txt')
 assert.ok($p.isStream(process.stdout))
 assert.ok($p.isStream(process.stdin))
+assert.ok($p.isStream(stream))
 assert.ok($p.isStream(file))
 
 // a normal stream is not a procstream
 assert.ok(!$p.is(process.stdout))
+assert.ok(!$p.is(stream))
+assert.ok(!$p.is())
 assert.ok(!$p.is(file))
 
 var proc = $p('echo "test"')
+  , pstream = $p.enhanceStream(stream)
 assert.ok($p.is(proc))
+assert.ok($p.is(pstream))
 // a procstream is also a process
 assert.ok($p.isProcess(proc))
 // a procstream is not like a normal stream
 assert.ok(!$p.isStream(proc))
+assert.ok(!$p.isProcess(pstream))
+assert.ok(!$p.isStream(pstream))
