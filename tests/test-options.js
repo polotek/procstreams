a7d7a5b6be88c092b5d734a2374727f547d04b07
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 16:35:19 -0700

    convert options tests to tap

--- tests/test-options.js
+++ tests/test-options.js
@@ -1,36 +1,49 @@
-var assert = require('assert')
+var test = require('tap').test
   , timers = require('./timers')
   , Collector = require('data-collector-stream')
   , $p = require(__dirname + '/..')
 
-var t = timers.multiTimer(3)
-$p('node tests/bin/out-test2.js')
-  .data(function(err, stdout) {
-    assert.ifError(err)
+test('out option sends stdout to process', function(assert) {
+  var t = timers.timer()
+  $p('node tests/bin/out-test2.js')
+    .data(function(err, stdout) {
+      assert.ifError(err)
 
-    t.stop()
-    assert.equal('output 2', stdout.toString().trim())
-  })
-
-var env = { 'ENV_TEST': 'env overridden' }
-Object.keys(process.env).forEach(function(k) {
-  env[k] = process.env[k]
+      t.stop()
+      assert.equal('output 2', stdout.toString().trim())
+      assert.end()
+    })
 })
 
-$p('node tests/bin/env-test.js'
-  , {
-    env: env
-  })
-  .data(function(err, stdout) {
-    assert.ifError(err)
+test('env option overrides proc environment', function(assert) {
+  var t = timers.timer()
 
-    t.stop()
-    assert.equal('env overridden', stdout.toString().trim())
+  var env = { 'ENV_TEST': 'env overridden' }
+  Object.keys(process.env).forEach(function(k) {
+    env[k] = process.env[k]
   })
 
-var collector = new Collector()
-collector.on('end', function() {
-  t.stop()
-  assert.equal('stderr output test', collector.getData().toString().trim())
+  $p('node tests/bin/env-test.js'
+    , {
+      env: env
+    })
+    .data(function(err, stdout) {
+      assert.ifError(err)
+
+      t.stop()
+      assert.equal('env overridden', stdout.toString().trim())
+      assert.end()
+    })
 })
-$p('node tests/bin/err-test.js').pipe('cat', { stderr: collector })
+
+test('stderr option sends stderr to provided stream', function(assert) {
+  var t = timers.timer()
+
+  var collector = new Collector()
+  collector.on('end', function() {
+    t.stop()
+    assert.equal('stderr output test', collector.getData().toString().trim())
+    assert.end()
+  })
+  $p('node tests/bin/err-test.js').pipe('cat', { stderr: collector })
+})
