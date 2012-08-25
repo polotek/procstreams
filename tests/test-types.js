9201969ceef5f691ea455858247ea4a8d26d8f55
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 16:35:39 -0700

    convert type tests to tap

--- tests/test-types.js
+++ tests/test-types.js
@@ -1,4 +1,4 @@
-var assert = require('assert')
+var test = require('tap').test
   , Stream = require('stream')
   , fs = require('fs')
   , cp = require('child_process')
@@ -6,37 +6,41 @@ var assert = require('assert')
   , spawn = cp.spawn
   , $p = require(__dirname + '/..')
 
-var child = spawn('echo "test"')
-  , child2 = exec('echo "test" | cat')
+test('the types behave properly', function(assert) {
+  var child = spawn('echo "test"')
+    , child2 = exec('echo "test" | cat')
 
-assert.ok($p.isProcess(process))
-assert.ok($p.isProcess(child))
-assert.ok($p.isProcess(child2))
+  assert.ok($p.isProcess(process), 'process is a process')
+  assert.ok($p.isProcess(child), 'spawn returns a process')
+  assert.ok($p.isProcess(child2), 'exec returns a process')
 
-// a process is not a procstream
-assert.ok(!$p.is(process))
-assert.ok(!$p.is(child));
+  // a process is not a procstream
+  assert.ok(!$p.is(process), 'process is not a procstream')
+  assert.ok(!$p.is(child), 'child_process is not a procstream');
 
-var stream = new Stream()
-  , file = fs.createReadStream('tests/fixtures/3lines.txt')
-assert.ok($p.isStream(process.stdout))
-assert.ok($p.isStream(process.stdin))
-assert.ok($p.isStream(stream))
-assert.ok($p.isStream(file))
+  var stream = new Stream()
+    , file = fs.createReadStream('tests/fixtures/3lines.txt')
+  assert.ok($p.isStream(process.stdout), 'process stdout is a stream')
+  assert.ok($p.isStream(process.stdin), 'process stdin is a stream')
+  assert.ok($p.isStream(stream), 'stream is a stream')
+  assert.ok($p.isStream(file), 'filestream is a stream')
 
-// a normal stream is not a procstream
-assert.ok(!$p.is(process.stdout))
-assert.ok(!$p.is(stream))
-assert.ok(!$p.is())
-assert.ok(!$p.is(file))
+  // a normal stream is not a procstream
+  assert.ok(!$p.is(process.stdout), 'process streams are not procstreams')
+  assert.ok(!$p.is(stream), 'stream is not a procstream')
+  assert.ok(!$p.is(file), 'file stream is not a procstream')
+  assert.ok(!$p.is(), "falsy check doesn't error")
 
-var proc = $p('echo "test"')
-  , pstream = $p.enhanceStream(stream)
-assert.ok($p.is(proc))
-assert.ok($p.is(pstream))
-// a procstream is also a process
-assert.ok($p.isProcess(proc))
-// a procstream is not like a normal stream
-assert.ok(!$p.isStream(proc))
-assert.ok(!$p.isProcess(pstream))
-assert.ok(!$p.isStream(pstream))
+  var proc = $p('echo "test"')
+    , pstream = $p.enhanceStream(stream)
+  assert.ok($p.is(proc), 'procstream is a procstream')
+  assert.ok($p.is(pstream), 'enhanced stream is a procstream')
+  // a procstream is also a process
+  assert.ok($p.isProcess(proc), 'procstream is a process')
+  // a procstream is not like a normal stream
+  assert.ok(!$p.isStream(proc), 'procstream is not a normal stream')
+  assert.ok(!$p.isProcess(pstream), 'enhanced stream is not a process')
+  assert.ok(!$p.isStream(pstream), 'enhanced stream is not a normal stream')
+
+  assert.end()
+})
