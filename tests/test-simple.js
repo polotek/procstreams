0917797e8ca1b13ecb9016079654cf42ee85779d
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 16:53:40 -0700

    convert simple tests to tap

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -1,30 +1,45 @@
-var assert = require('assert')
-  , multiTimer = require(__dirname + '/timers').multiTimer
+var test = require('tap').test
+  , timers = require(__dirname + '/timers')
   , exec = require('child_process').exec
   , $p = require(__dirname + '/..')
 
-var version = process.version
-exec('node --version', function(err, output){
-  assert.ifError(err)
+test('flexible arguments', function(assert) {
+  var version = process.version
+  exec('node --version', function(err, output){
+    assert.ifError(err)
 
-  assert.equal(version, output.toString().trim())
+    assert.equal(version, output.toString().trim())
 
-  var t = multiTimer(3)
-  $p('node --version')
-    .data(function(err, output) {
-      t.stop()
-      assert.equal(version, output.toString().trim())
+    assert.test('string cmd with args', function(assert) {
+      var t = timers.timer()
+      $p('node --version')
+        .data(function(err, output) {
+          t.stop()
+          assert.equal(version, output.toString().trim())
+          assert.end()
+        })
     })
 
-  $p('node',  '--version')
-    .data(function(err, output) {
-      t.stop()
-      assert.equal(version, output.toString().trim())
+    assert.test('string cmd, string args', function(assert) {
+      var t = timers.timer()
+      $p('node',  '--version')
+        .data(function(err, output) {
+          t.stop()
+          assert.equal(version, output.toString().trim())
+          assert.end()
+        })
     })
 
-  $p('node',  ['--version'])
-    .data(function(err, output) {
-      t.stop()
-      assert.equal(version, output.toString().trim())
+    assert.test('string cmd, array args', function(assert) {
+      var t = timers.timer()
+      $p('node',  ['--version'])
+        .data(function(err, output) {
+          t.stop()
+          assert.equal(version, output.toString().trim())
+          assert.end()
+        })
     })
+
+    assert.end()
+  })
 })
