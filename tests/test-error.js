cebe83e97aaca78d9fb4d9d3649c70cbd9615111
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 18:25:55 -0700

    convert error tests to tap

--- tests/test-error.js
+++ tests/test-error.js
@@ -1,48 +1,63 @@
-var assert = require('assert')
+var test = require('tap').test
   , fs = require('fs')
-  , multiTimer = require(__dirname + '/timers').multiTimer
+  , timers = require(__dirname + '/timers')
   , exec = require('child_process').exec
   , $p = require(__dirname + '/..')
 
-exec('node does-not-exist', function(err, stdout, stderr) {
-  assert.ok(err)
+test('proc errors', function(assert) {
+  exec('node does-not-exist', function(err, stdout, stderr) {
+    assert.ok(err)
 
-  var t = multiTimer(3)
-  $p('node does-not-exist')
-    .data(function(err, stdout, stderr) {
-      t.stop()
+    assert.test('data method returns error', function(assert) {
+      var t = timers.timer()
+      $p('node does-not-exist')
+        .data(function(err, stdout, stderr) {
+          t.stop()
 
-      assert.ok(err)
-      assert.notStrictEqual(0, err.code)
-      assert.ok(stderr && /does-not-exist/.test(stderr))
+          assert.ok(err)
+          assert.notEqual(0, err.code)
+          assert.ok(stderr && /does-not-exist/.test(stderr))
+          assert.end()
+        })
     })
 
-  var err = null
-  $p('node does-not-exist')
-    .on('error', function(_err) {
-      err = _err
-    })
-    .on('close', function(errCode) {
-      t.stop()
-      assert.ok(err)
-      assert.notStrictEqual(0, err.code)
-      assert.strictEqual(errCode, err.code)
-    })
-
-  var err = null
-    , proc = null
-  proc = $p('node does-not-exist')
-    .on('error', function(_err) {
-      err = _err
-    })
-    .on('close', function(errCode, signal) {
-      t.stop()
-      assert.ok(err)
-      assert.ok(err.signal)
-      assert.strictEqual(signal, err.signal)
+    assert.test('error event raised with code', function(assert) {
+      var t = timers.timer()
+      var err = null
+      $p('node does-not-exist')
+        .on('error', function(_err) {
+          err = _err
+        })
+        .on('close', function(errCode) {
+          t.stop()
+          assert.ok(err)
+          assert.notEqual(0, err.code)
+          assert.equal(errCode, err.code)
+          assert.end()
+        })
     })
 
-  process.nextTick(function() {
-    proc.kill()
+    assert.test('error event raised with kill signal', function(assert) {
+      var t = timers.timer()
+      var err = null
+        , proc = null
+      proc = $p('node does-not-exist')
+        .on('error', function(_err) {
+          err = _err
+        })
+        .on('close', function(errCode, signal) {
+          t.stop()
+          assert.ok(err)
+          assert.ok(err.signal)
+          assert.equal(signal, err.signal)
+          assert.end()
+        })
+
+      process.nextTick(function() {
+        proc.kill()
+      })
+    })
+
+    assert.end()
   })
 })
