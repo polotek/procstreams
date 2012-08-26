4ca0a9b31e502c39ef33bb6c2733da7a57243c74
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 18:06:09 -0700

    convert promise tests to tap

--- tests/test-promises.js
+++ tests/test-promises.js
@@ -1,55 +1,90 @@
-var assert = require('assert')
+var test = require('tap').test
   , fs = require('fs')
-  , multiTimer = require(__dirname + '/timers').multiTimer
+  , timers = require(__dirname + '/timers')
   , exec = require('child_process').exec
   , $p = require(__dirname + '/..')
   , nop = function() {}
 
 
-var t = multiTimer(5)
-$p('echo one')
-  .and('echo two')
-  .and('echo three')
-    .data(function(err, stdout, stderr) {
+test('proc promises', function(assert) {
+  assert.test('and method on promise fires on success', function(assert) {
+    var t = timers.timer()
+    $p('echo one')
+      .and('echo two')
+      .and('echo three')
+        .data(function(err, stdout, stderr) {
+          t.stop()
+          assert.equal('three', stdout.toString().trim())
+          assert.end()
+        })
+  })
+
+  assert.test('or method on promise fires on failure', function(assert) {
+    var t = timers.timer()
+    $p('echo one')
+      .and('fail two').on('error', nop)
+      .or('echo three')
+        .data(function(err, stdout, stderr) {
+          t.stop()
+          assert.equal('three', stdout.toString().trim())
+          assert.end()
+        })
+  })
+
+  assert.test('then method on promise fires on success', function(assert) {
+    var t = timers.timer()
+    $p('echo one')
+      .then('echo two')
+      .then('echo three')
+        .data(function(err, stdout, stderr) {
+          t.stop()
+          assert.equal('three', stdout.toString().trim())
+          assert.end()
+        })
+  })
+
+  assert.test('then promise fires on failure', function(assert) {
+    var t = timers.timer()
+    $p('echo one')
+      .then('fail two').on('error', nop)
+      .then('echo three')
+        .data(function(err, stdout, stderr) {
+          t.stop()
+          assert.equal('three', stdout.toString().trim())
+          assert.end()
+        })
+  })
+
+  assert.test('promise events are transfered to procstream', function(assert) {
+    var t = timers.timer()
+    var outProc
+    $p('echo one')
+      .and('echo two')
+      .and('echo three')
+        .on('start', function() {
+          outProc = this
+        })
+        .out()
+
+    process.stdout.on('pipe', function(source) {
       t.stop()
-      assert.equal('three', stdout.toString().trim())
+      assert.strictEqual(source, outProc.stdout)
+      assert.end()
     })
+  })
 
-$p('echo one')
-  .and('fail two').on('error', nop)
-  .or('echo three')
-    .data(function(err, stdout, stderr) {
-      t.stop()
-      assert.equal('three', stdout.toString().trim())
-    })
+  assert.test('promises can be piped', function(assert) {
+    var t = timers.timer()
+    $p('echo one')
+      .and('echo two')
+      .and('echo three')
+      .pipe('cat')
+        .data(function(err, stdout, stderr) {
+          t.stop()
+          assert.equal('three', stdout.toString().trim())
+          assert.end()
+        })
+  })
 
-$p('echo one')
-  .then('echo two')
-  .then('echo three')
-    .data(function(err, stdout, stderr) {
-      t.stop()
-      assert.equal('three', stdout.toString().trim())
-    })
-
-var outProc
-$p('echo one')
-  .and('echo two')
-  .and('echo three')
-    .on('start', function() {
-      outProc = this
-    })
-    .out()
-
-process.stdout.on('pipe', function(source) {
-  t.stop()
-  assert.strictEqual(source, outProc.stdout)
+  assert.end()
 })
-
-$p('echo one')
-  .and('echo two')
-  .and('echo three')
-  .pipe('cat')
-    .data(function(err, stdout, stderr) {
-      t.stop()
-      assert.equal('three', stdout.toString().trim())
-    })
