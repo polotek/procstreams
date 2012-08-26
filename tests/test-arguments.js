4a16f11d93534f2be26c88a47531f371881d5121
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 18:47:57 -0700

    convert argument tests to tap

--- tests/test-arguments.js
+++ tests/test-arguments.js
@@ -1,66 +1,95 @@
-var assert = require('assert')
-  , multiTimer = require('./timers').multiTimer
+var test = require('tap').test
+  , timers = require('./timers')
   , exec = require('child_process').exec
   , $p = require(__dirname + '/..')
 
-var version = process.version
-exec('node --version', function(err, output){
-  assert.ifError(err)
+test('flexible arguments', function(assert) {
+  var version = process.version
+  exec('node --version', function(err, output){
+    assert.ifError(err)
 
-  assert.equal(version, output.toString().trim())
+    assert.equal(version, output.toString().trim())
 
-  var t = multiTimer(11)
-  $p('node --version')
-    .data(function(err, output) {
-      t.stop()
-      assert.equal(version, output.toString().trim())
+    assert.test('string cmd with args', function(assert) {
+      var t = timers.timer()
+      $p('node --version')
+        .data(function(err, output) {
+          t.stop()
+          assert.equal(version, output.toString().trim())
+          assert.end()
+        })
     })
 
-  $p('node',  '--version')
-    .data(function(err, output) {
-      t.stop()
-      assert.equal(version, output.toString().trim())
+    assert.test('string cmd, string args', function(assert) {
+      var t = timers.timer()
+      $p('node',  '--version')
+        .data(function(err, output) {
+          t.stop()
+          assert.equal(version, output.toString().trim())
+          assert.end()
+        })
     })
 
-  $p('node',  ['--version'])
-    .data(function(err, output) {
-      t.stop()
-      assert.equal(version, output.toString().trim())
+    assert.test('string cmd, array args', function(assert) {
+      var t = timers.timer()
+      $p('node',  ['--version'])
+        .data(function(err, output) {
+          t.stop()
+          assert.equal(version, output.toString().trim())
+          assert.end()
+        })
     })
 
-  $p('node --version', function() {
-    t.stop()
-  })
+    assert.test('argument combinations', function(assert) {
+      var t = timers.multiTimer(6, function() {
+        assert.end()
+      })
 
-  $p('node', '--version', function() {
-    t.stop()
-  })
+      $p('node --version', function() {
+        t.stop()
+      })
 
-  $p('node --version', null, null, function() {
-    t.stop()
-  })
+      $p('node', '--version', function() {
+        t.stop()
+      })
 
-  $p('node --version', function() {
-    t.stop()
-  })
+      $p('node --version', null, null, function() {
+        t.stop()
+      })
 
-  $p('node --version', { out: true }, function() {
-    t.stop()
-  })
+      $p('node --version', function() {
+        t.stop()
+      })
 
-  $p('node', '--version', { out: true }, function() {
-    t.stop()
-  })
+      $p('node --version', { out: true }, function() {
+        t.stop()
+      })
 
-  $p('echo "new\nline"')
-    .data(function(err, output) {
-      t.stop()
-      assert.equal('new\nline', output.toString().trim())
+      $p('node', '--version', { out: true }, function() {
+        t.stop()
+      })
     })
 
-  $p('echo "foo" "bar baz" \'fizz buzz\'')
-    .data(function(err, output) {
-      t.stop()
-      assert.equal('foo bar baz fizz buzz', output.toString().trim())
+    assert.test('args with quotes and newlines', function(assert) {
+      var t = timers.timer()
+      $p('echo "new\nline"')
+        .data(function(err, output) {
+          t.stop()
+          assert.equal('new\nline', output.toString().trim())
+          assert.end()
+        })
     })
+
+    assert.test('non-trivial args', function(assert) {
+      var t = timers.timer()
+      $p('echo "foo" "bar baz" \'fizz buzz\'')
+        .data(function(err, output) {
+          t.stop()
+          assert.equal('foo bar baz fizz buzz', output.toString().trim())
+          assert.end()
+        })
+    })
+
+    assert.end()
+  })
 })
