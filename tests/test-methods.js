8aecc55fe09d4b2bd27f34c52979531adf40eb75
Marco Rogers <marco.rogers@gmail.com>
2012-08-25 17:01:37 -0700

    convert method tests to tap

--- tests/test-methods.js
+++ tests/test-methods.js
@@ -1,26 +1,37 @@
-var assert = require('assert')
+var test = require('tap').test
   , fs = require('fs')
-  , timer = require(__dirname + '/timers').timer
+  , timers = require(__dirname + '/timers')
   , exec = require('child_process').exec
   , $p = require(__dirname + '/..')
 
-var processStdout = ''
-process.stdout.on('data', function(d) {
-  processStdout += d
+test('data method returns combined output', function(assert) {
+  var processStdout = ''
+  process.stdout.on('data', function(d) {
+    processStdout += d
+  })
+
+  fs.readFile('tests/fixtures/long.txt', function(err, fileData) {
+    assert.ifError(err)
+
+    var t = timers.timer()
+    $p('cat tests/fixtures/long.txt')
+      .data(function(err, stdout, stderr) {
+        assert.ifError(err)
+ 
+        t.stop()
+        assert.equal(fileData.toString(), stdout.toString())
+        assert.end()
+      })
+  })
 })
 
-fs.readFile('tests/fixtures/long.txt', function(err, fileData) {
-  assert.ifError(err)
-
-  var t = timer()
-  $p('cat tests/fixtures/long.txt')
-    .data(function(err, stdout, stderr) {
-      t.stop()
-      assert.equal(fileData.toString(), stdout.toString())
-    })
-})
-
-exec('node tests/bin/out-test.js', function(err, output) {
-  assert.ifError(err)
-  assert.equal('output 3', output.toString().trim())
+test('out method sends stdout to process', function(assert) {
+  var t = timers.timer()
+  exec('node tests/bin/out-test.js', function(err, output) {
+    assert.ifError(err)
+ 
+    t.stop()
+    assert.equal('output 3', output.toString().trim())
+    assert.end()
+  })
 })
